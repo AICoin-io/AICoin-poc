86b8444e0b594270103022c0262e5d5c8ab78fe8
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-06-07 21:40:43 +1000

    Remove unused methods.

--- src/contracts/SingleVote.sol
+++ src/contracts/SingleVote.sol
@@ -2,34 +2,58 @@ pragma solidity ^0.4.8;
 
 contract SingleVote {
 
+    uint256 public totalSupply;
     address public owner;
-
-    struct Member {
-        address member;
-        uint weight;
-        bool voted;
-        uint proposalIndex;
-    }
-
-    struct Proposal {
-        bytes32 name;
-        uint256 start;
-        uint256 end;
-        uint voteCount;
-    }
-
-    //Members
-    mapping(address=>Member) public members;
-
-    //Proposal options
-    Proposal[] public proposals;
 
     function SingleVote()
     {
         owner = msg.sender;
+        totalSupply = 100000;
+
+        balances[msg.sender] = totalSupply;
     }
 
-    /// Create a new ballot to choose one of `proposalNames`.
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    // function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    //     if (balances[_from] >= _value && _value > 0) {
+    //         balances[_to] += _value;
+    //         balances[_from] -= _value;
+    //         Transfer(_from, _to, _value);
+    //         return true;
+    //     } else { return false; }
+    // }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    mapping (address => uint256) balances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    //voting
+    struct Proposal {
+        bytes32 name;
+        uint256 start;
+        uint256 end;
+        uint256 voteCount;
+    }
+
+    struct Member {
+        address member;
+        bool voted;
+        //uint proposalIndex;
+    }
+
+        /// Create a new ballot to choose one of `proposalNames`.
     function AddBallot(bytes32[] proposalNames, uint256 _start, uint256 _end) {
         //Only contract owner can excute
         if (msg.sender == owner) {
@@ -45,47 +69,24 @@ contract SingleVote {
     }
     
     //vote on the proposal index
-    function vote(uint proposalIndex) 
+    function Vote(uint proposalIndex) 
     {
         //Check to see if member is registered
         //if (members[msg.sender].length != 0)
         //{
             Member sender = members[msg.sender];
             sender.voted = true;
-            sender.proposalIndex = proposalIndex;
 
             Proposal proposal = proposals[proposalIndex];
-            proposal.voteCount += sender.weight;
+
+            uint256 weight = balanceOf(msg.sender);
+            proposal.voteCount += weight;
         //}
     }
 
-    function addWeight(address delegate, uint weight)
-    {
-        //Only contract owner can excute
-        if (msg.sender == owner) {
-            Member member = members[delegate];
-            member.weight += weight;
-        }
-    } 
+    //Members
+    mapping(address=>Member) public members;
 
-    function removeWeight(address delegate, uint weight)
-    {
-        //Only contract owner can excute
-        if (msg.sender == owner) {
-            Member member = members[delegate];
-            member.weight -= weight;
-        }
-    }
-
-    function addMembers(address member, uint weight) {
-        //Only contract owner can excute
-        if (msg.sender == owner) {
-            Member memory newMember = Member(member, weight, false, 0);
-            members[member] = newMember;
-        }
-    }
-
-    // function removeMember(address member) {
-
-    // }
+    //Proposal options
+    Proposal[] public proposals;
 }
